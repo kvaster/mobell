3d53314ea99ea4fc3f24529390226feaf93e12b7
Viktor Kuzmin <kvaster@gmail.com>
2021-06-28 15:50:53 +0300

    Upgrade gradle, remove unused files

--- app/src/androidTest/java/com/kvaster/mobell/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kvaster/mobell/ExampleInstrumentedTest.java
@@ -1,13 +1,12 @@
 package com.kvaster.mobell;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,7 +20,7 @@ public class ExampleInstrumentedTest
     public void useAppContext()
     {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
         assertEquals("com.kvaster.mobell", appContext.getPackageName());
     }
